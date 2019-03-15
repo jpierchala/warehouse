dbed3f0bddadfc57e0f2c71ae8be106cbddad27f
jaroslaw <jpierchala@protonmail.com>
2019-03-15 01:30:46 +0100

    clean

--- src/main/java/com/ps/project/warehouse/config/bootstrap/BootstrapProductsAndProductTypes.java
+++ src/main/java/com/ps/project/warehouse/config/bootstrap/BootstrapProductsAndProductTypes.java
@@ -1,19 +1,8 @@
 package com.ps.project.warehouse.config.bootstrap;
 
-import com.ps.project.warehouse.Repository.RoleRepository;
-import com.ps.project.warehouse.Repository.UserRepository;
-import com.ps.project.warehouse.domain.Role;
-import com.ps.project.warehouse.domain.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
-import java.util.HashSet;
-import java.util.Set;
 
 @Service
 @Slf4j
@@ -21,6 +10,6 @@ public class BootstrapProductsAndProductTypes implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        
+
     }
 }
